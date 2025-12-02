6b9753c3dd1051400f11c8e3ac1f9da5bed0b932
Deeraj Kumar <deerajkumar79899@gmail.com>
2025-12-02 06:34:19 +0530

    Updated build script to set Python version for Render

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Force Render to use Python 3.12.1
+echo "3.12.1" > .python-version
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
